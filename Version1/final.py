8a62688ce685cde14264bf6b2d3536ffdacf9b56
Sseptimus <69606116+Sseptimus@users.noreply.github.com>
2022-07-27 20:41:02 +1200

    began on final

--- Version1/final.py
+++ Version1/final.py
@@ -1,8 +1,17 @@
-from tkinter import *
 import tkinter as tk
+from tkinter import *
+from random import *
 import json
 import math
 from functools import partial
+
+login_page = tk.Tk()
+login_wallpapers = [r"Password-Generator\Assets\login_wallpapers\cave_edited.png",r"Password-Generator\Assets\login_wallpapers\tower_edited.png"]
+headerBackground = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\header_background.png")
+tabBackground = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\tab_background.png")
+new_password_Background = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\new_password.png")
+backGroundImage=PhotoImage(file=choice(login_wallpapers))
+
 with open('Password-Generator\Version1\Password-Generator-Data.json', 'r') as f:
   data = json.load(f)
   
@@ -18,13 +27,10 @@ show_buttons = []
 current_page = 1
 total_pages = 0
 
-manager = tk.Tk()
-headerBackground = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\header_background.png")
-tabBackground = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\tab_background.png")
-new_password_Background = PhotoImage(file=r"Password-Generator\Assets\Element backgrounds\new_password.png")
-
 
 def setup_manager():
+    global manager
+    manager = Toplevel(login_page)
     manager.title("Password Manager")
     manager.configure(background="#0e0118")
     manager.bind("<Escape>", lambda event: manager.destroy())
@@ -199,7 +205,41 @@ def popup(location):
     password_info = Label(info_window, text=passwords[location],font=("Arial", 20),fg="#ffffff", background="#0e0118")
     password_info.place(x=25,y=245)
     
+def login():
+    global password_entry, username_entry
+    login_page.title('Password Manager')
+    login_page.configure(background='black')
+    screen_width = login_page.winfo_screenwidth()
+    screen_height = login_page.winfo_screenheight()
+    geometry = "500x500+" + str(int(screen_width/2 - 250)) + "+" + str(int(screen_height/2 - 250))
+    login_page.geometry(geometry)
+    login_wallpaper = Label(login_page,image=backGroundImage)
+    login_wallpaper.place(x=-2,y=-2)
+    username_label = Label(login_page,text="Username:",bg='black',fg='white',font=('Arial',20))
+    username_label.place(x=150,y=140)
+    username_entry = Entry(login_page,width=20,font=('Arial',15))
+    username_entry.place(x=150,y=185)
+    password_label = Label(login_page,text="Password:",bg='black',fg='white',font=('Arial',20))
+    password_label.place(x=150,y=250)
+    password_entry = Entry(login_page,width=20,font=('Arial',15),show="*")
+    password_entry.place(x=150,y=295)
+    enter_button = Button(login_page,text="Enter",font=("Arial", 15), background="#0e0118", foreground="#ffffff",command=login_check)
+    enter_button.place(x=150,y=330)
+    login_page.bind("<Escape>", lambda event: login_page.destroy())
+
+def login_check():
+    username = username_entry.get()
+    password = password_entry.get()
+    if username.lower() == "admin" and password == "password":
+        login_page.withdraw()
+        setup_manager()
+
+    else:
+        
+        login_page.messagebox.showerror(title="Error",message="Incorrect Username or Password")
+        username_entry.delete(0,END)
+        password_entry.delete(0,END)
+        
 if __name__ == "__main__":
-    setup_manager()
-    manager.mainloop()
-    
+    login()
+    login_page.mainloop()
--- Version1/part1.py
+++ Version1/part1.py
@@ -1,35 +1,40 @@
+import tkinter as tk
 from tkinter import *
 from random import *
 
-login_wallpapers = ["Password-Generator\Assets\login_wallpapers\cave_edited_2.png",r"Password-Generator\Assets\login_wallpapers\tower_edited.png"]
-class Login(Tk):
-    def setup(self):
-        self.title('Password Manager')
-        self.configure(background='black')
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        geometry = "500x500+" + str(int(screen_width/2 - 250)) + "+" + str(int(screen_height/2 - 250))
-        self.geometry(geometry)
-        self.backGroundImage=PhotoImage(file=choice(login_wallpapers))
-        self.login_wallpaper = Label(self,image=self.backGroundImage)
-        self.login_wallpaper.place(x=-2,y=-2)
-        self.username_entry = Entry(self,width=20,font=('Times New Roman',15))
-        self.username_entry.place(x=150,y=185)
-        self.password_entry = Entry(self,width=20,font=('Times New Roman',15),show="*")
-        self.password_entry.place(x=150,y=295)
-        self.enter_button = Button(self,text="Enter",width=10,height=2,font=('Times New Roman',15),command=self.login)
-        self.enter_button.place(x=150,y=400)
-        self.bind("<Escape>", lambda event: self.destroy())
-    def login(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        if username.lower() == "admin" and password == "password":
-            print("access granted")
-        else:
-            print("access denied")
+login_wallpapers = [r"Password-Generator\Assets\login_wallpapers\cave_edited.png",r"Password-Generator\Assets\login_wallpapers\tower_edited.png"]
+page=tk.Tk()
+backGroundImage=PhotoImage(file=choice(login_wallpapers))
+def login(login_page):
+    global password_entry, username_entry
+    login_page.title('Password Manager')
+    login_page.configure(background='black')
+    screen_width = login_page.winfo_screenwidth()
+    screen_height = login_page.winfo_screenheight()
+    geometry = "500x500+" + str(int(screen_width/2 - 250)) + "+" + str(int(screen_height/2 - 250))
+    login_page.geometry(geometry)
+    login_wallpaper = Label(login_page,image=backGroundImage)
+    login_wallpaper.place(x=-2,y=-2)
+    username_label = Label(login_page,text="Username:",bg='black',fg='white',font=('Arial',20))
+    username_label.place(x=150,y=140)
+    username_entry = Entry(login_page,width=20,font=('Arial',15))
+    username_entry.place(x=150,y=185)
+    password_label = Label(login_page,text="Password:",bg='black',fg='white',font=('Arial',20))
+    password_label.place(x=150,y=250)
+    password_entry = Entry(login_page,width=20,font=('Arial',15),show="*")
+    password_entry.place(x=150,y=295)
+    enter_button = Button(login_page,text="Enter",font=("Arial", 15), background="#0e0118", foreground="#ffffff",command=login_check)
+    enter_button.place(x=150,y=330)
+    login_page.bind("<Escape>", lambda event: login_page.destroy())
+def login_check():
+    username = username_entry.get()
+    password = password_entry.get()
+    if username.lower() == "admin" and password == "password":
+        print("access granted")
+    else:
+        print("access denied")
 if __name__ =="__main__":
-    login_page = Login()
-    login_page.setup()
+    login(page)
+    page.mainloop()
     
-while True:
-    login_page.update()
+    
